804302ed73e5600d846e729f3866bfb3d832379d
Tobias Wältken <tobias.w@ltken.de>
2016-03-03 12:39:10 +0100

    added missing type to php

--- sql.php
+++ sql.php
@@ -29,9 +29,9 @@ define("SQL_CREATE_DATABASE_DESCRIPTIONS", json_encode(array(
 define("SQL_CREATE_DATABASE_COMMANDS", json_encode(array(
 	"CREATE TABLE Address (id tinyint NOT NULL AUTO_INCREMENT, dns bit(1) DEFAULT 0 NOT NULL, dhcp bit(1) DEFAULT 0 NOT NULL, dynamic bit(1) DEFAULT 0 NOT NULL, Computer_id int(10) UNIQUE, PRIMARY KEY (id), UNIQUE INDEX (id));",
 	"CREATE TABLE Computer (id int(10) NOT NULL AUTO_INCREMENT, mac varchar(17) NOT NULL UNIQUE, name varchar(255) UNIQUE, location varchar(255), type varchar(255), `function` varchar(255), production bit(1) DEFAULT 0 NOT NULL, manager varchar(255), PRIMARY KEY (id), UNIQUE INDEX (id));",
-	"CREATE TABLE Key (id unsigned NOT NULL AUTO_INCREMENT, `key` text, type varchar(5), fingerprint varchar(47), Login_id unsigned, PRIMARY KEY (id), UNIQUE INDEX (id));",
-	"CREATE TABLE Login (id unsigned NOT NULL AUTO_INCREMENT, username varchar(255) NOT NULL, password varchar(255) NOT NULL, Computer_id int(10) NOT NULL, PRIMARY KEY (id), UNIQUE INDEX (id));",
-	"CREATE TABLE Pool (id unsigned NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, start_Address tinyint NOT NULL, end_Address tinyint NOT NULL, PRIMARY KEY (id), UNIQUE INDEX (id));",
+	"CREATE TABLE Key (id int(10) unsigned NOT NULL AUTO_INCREMENT, `key` text, type varchar(5), fingerprint varchar(47), Login_id unsigned, PRIMARY KEY (id), UNIQUE INDEX (id));",
+	"CREATE TABLE Login (id int(10) unsigned NOT NULL AUTO_INCREMENT, username varchar(255) NOT NULL, password varchar(255) NOT NULL, Computer_id int(10) NOT NULL, PRIMARY KEY (id), UNIQUE INDEX (id));",
+	"CREATE TABLE Pool (id int(10) unsigned NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, start_Address tinyint NOT NULL, end_Address tinyint NOT NULL, PRIMARY KEY (id), UNIQUE INDEX (id));",
 	"ALTER TABLE Pool ADD INDEX FKPool851427 (start_Address), ADD CONSTRAINT FKPool851427 FOREIGN KEY (start_Address) REFERENCES Address (id);",
 	"ALTER TABLE Pool ADD INDEX FKPool715931 (end_Address), ADD CONSTRAINT FKPool715931 FOREIGN KEY (end_Address) REFERENCES Address (id);",
 	"ALTER TABLE Address ADD INDEX FKAddress312723 (Computer_id), ADD CONSTRAINT FKAddress312723 FOREIGN KEY (Computer_id) REFERENCES Computer (id);",
