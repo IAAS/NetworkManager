c68033f58689adc42686b19af76ec544faf4d43c
Tobias Wältken <tobias.w@ltken.de>
2016-03-03 15:12:31 +0100

    renamed a lot

--- sql.php
+++ sql.php
@@ -16,26 +16,26 @@ define("DEBUG", true);
 define("SQL_CREATE_DATABASE_DESCRIPTIONS", json_encode(array(
 	"create Address table",
 	"create Computer table",
-	"create Key table",
+	"create SSHKey table",
 	"create Login table",
 	"create Pool table",
 	"create foreign key on Pool table",
 	"create foreign key on Pool table",
 	"create foreign key on Address table",
 	"create foreign key on Login table",
-	"create foreign key on Key table"
+	"create foreign key on SSHKey table"
 )));
 define("SQL_CREATE_DATABASE_COMMANDS", json_encode(array(
 	"CREATE TABLE Address (id tinyint NOT NULL AUTO_INCREMENT, dns bit(1) DEFAULT 0 NOT NULL, dhcp bit(1) DEFAULT 0 NOT NULL, dynamic bit(1) DEFAULT 0 NOT NULL, Computer_id int(10) UNIQUE, PRIMARY KEY (id), UNIQUE INDEX (id));",
 	"CREATE TABLE Computer (id int(10) NOT NULL AUTO_INCREMENT, mac varchar(17) NOT NULL UNIQUE, name varchar(255) UNIQUE, location varchar(255), type varchar(255), `function` varchar(255), production bit(1) DEFAULT 0 NOT NULL, manager varchar(255), PRIMARY KEY (id), UNIQUE INDEX (id));",
-	"CREATE TABLE Key (id int(10) unsigned NOT NULL AUTO_INCREMENT, key TEXT, type varchar(5), fingerprint varchar(47), Login_id int(10) unsigned, PRIMARY KEY (id), UNIQUE INDEX (id));",
+	"CREATE TABLE SSHKey (id int(10) unsigned NOT NULL AUTO_INCREMENT, keyData TEXT, keyType varchar(5), fingerprint varchar(47), Login_id int(10) unsigned, PRIMARY KEY (id), UNIQUE INDEX (id));",
 	"CREATE TABLE Login (id int(10) unsigned NOT NULL AUTO_INCREMENT, username varchar(255) NOT NULL, password varchar(255) NOT NULL, Computer_id int(10) NOT NULL, PRIMARY KEY (id), UNIQUE INDEX (id));",
 	"CREATE TABLE Pool (id int(10) unsigned NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, start_Address tinyint NOT NULL, end_Address tinyint NOT NULL, PRIMARY KEY (id), UNIQUE INDEX (id));",
 	"ALTER TABLE Pool ADD INDEX FKPool851427 (start_Address), ADD CONSTRAINT FKPool851427 FOREIGN KEY (start_Address) REFERENCES Address (id);",
 	"ALTER TABLE Pool ADD INDEX FKPool715931 (end_Address), ADD CONSTRAINT FKPool715931 FOREIGN KEY (end_Address) REFERENCES Address (id);",
 	"ALTER TABLE Address ADD INDEX FKAddress312723 (Computer_id), ADD CONSTRAINT FKAddress312723 FOREIGN KEY (Computer_id) REFERENCES Computer (id);",
 	"ALTER TABLE Login ADD INDEX FKLogin947788 (Computer_id), ADD CONSTRAINT FKLogin947788 FOREIGN KEY (Computer_id) REFERENCES Computer (id);",
-	"ALTER TABLE Key ADD INDEX FKKey890017 (Login_id), ADD CONSTRAINT FKKey890017 FOREIGN KEY (Login_id) REFERENCES Login (id);"
+	"ALTER TABLE SSHKey ADD INDEX FKKey890017 (Login_id), ADD CONSTRAINT FKKey890017 FOREIGN KEY (Login_id) REFERENCES Login (id);"
 )));
 
 define("SQL_CLEAR_DATABASE_DESCRIPTIONS", json_encode(array(
@@ -43,10 +43,10 @@ define("SQL_CLEAR_DATABASE_DESCRIPTIONS", json_encode(array(
 	"drop foreign key on Pool table",
 	"drop foreign key on Address table",
 	"drop foreign key on Login table",
-	"drop foreign key on Key table",
+	"drop foreign key on SSHKey table",
 	"drop table Address",
 	"drop table Computer",
-	"drop table Key",
+	"drop table SSHKey",
 	"drop table Login",
 	"drop table Pool",
 )));
@@ -55,10 +55,10 @@ define("SQL_CLEAR_DATABASE_COMMANDS", json_encode(array(
 	"ALTER TABLE Pool DROP FOREIGN KEY FKPool715931;",
 	"ALTER TABLE Address DROP FOREIGN KEY FKAddress312723;",
 	"ALTER TABLE Login DROP FOREIGN KEY FKLogin947788;",
-	"ALTER TABLE Key DROP FOREIGN KEY FKKey890017;",
+	"ALTER TABLE SSHKey DROP FOREIGN KEY FKKey890017;",
 	"DROP TABLE IF EXISTS Address;",
 	"DROP TABLE IF EXISTS Computer;",
-	"DROP TABLE IF EXISTS Key;",
+	"DROP TABLE IF EXISTS SSHKey;",
 	"DROP TABLE IF EXISTS Login;",
 	"DROP TABLE IF EXISTS Pool;"
 )));
