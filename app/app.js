c22f2292bdecd0652aa3cc7cc5ecf9884572d24c
Tobias Wältken <tobias.w@ltken.de>
2016-03-12 00:22:52 +0100

    first try to make a semi usable site with angular js

---
+++ app/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('mainModule', []);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/',
+			{
+				controller: 'computerController',
+				templateUrl: '/app/partials/computerList.html'
+			})
+		.otherwise({ redirectTo: '/'});
+});
---
+++ app/controllers/controllers.js
@@ -0,0 +1,6 @@
+app.controller('computerController', function($scope, $http) {
+	$http.get("/sql/computers.php")
+		.then(function(response) {
+			$scope.computers = response.data.records;
+		});
+});
